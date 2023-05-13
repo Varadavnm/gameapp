166c0ad6bc5d33b1094376b612e7db4339159a0b
satrtingpyth <varadavnm@gmail.com>
2023-05-13 23:37:07 +0530

    Update .gitignore

--- dart/datatype/bin/datatype.dart
+++
@@ -1,5 +0,0 @@
-import 'package:datatype/datatype.dart' as datatype;
-
-void main(List<String> arguments) {
-  print('Hello world: ${datatype.calculate()}!');
-}
--- dart/datatype/test/datatype_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:datatype/datatype.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
